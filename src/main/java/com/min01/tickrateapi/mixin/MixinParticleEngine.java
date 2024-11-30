4204c5e8e94e603f6eac8665c73fb02d103ae10f
min01 <goyoumin2@gmail.com>
2024-11-30 09:35:55 +0900

    Update MixinParticleEngine.java

--- src/main/java/com/min01/tickrateapi/mixin/MixinParticleEngine.java
+++ src/main/java/com/min01/tickrateapi/mixin/MixinParticleEngine.java
@@ -4,11 +4,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.min01.tickrateapi.util.TickrateUtil;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 
+import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleEngine;
 
 @Mixin(ParticleEngine.class)
@@ -23,6 +27,19 @@ public class MixinParticleEngine
 		if(TickrateUtil.isDimensionTimeStopped(this.level.dimension()))
 		{
 			ci.cancel();
+		}
+	}
+	
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/Particle;render(Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/client/Camera;F)V"), method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V")
+	private void render(Particle instance, VertexConsumer p_107261_, Camera p_107262_, float p_107263_)
+	{
+		if(TickrateUtil.isDimensionTimeStopped(this.level.dimension()))
+		{
+			instance.render(p_107261_, p_107262_, TickrateUtil.STOP.partialTick);
+		}
+		else
+		{
+			instance.render(p_107261_, p_107262_, p_107263_);
 		}
 	}
 }
