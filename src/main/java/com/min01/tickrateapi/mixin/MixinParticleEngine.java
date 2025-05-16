f4da8c07952262feada53cdfe850a98ff03b926b
min01 <goyoumin2@gmail.com>
2025-05-16 20:42:17 +0900

    Revert "WIP"
    This reverts commit 039873cbb5e4b8eaad03086d18f2dff3a822f8ab.

--- src/main/java/com/min01/tickrateapi/mixin/MixinParticleEngine.java
+++ src/main/java/com/min01/tickrateapi/mixin/MixinParticleEngine.java
@@ -7,13 +7,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.min01.tickrateapi.config.TimerConfig;
 import com.min01.tickrateapi.util.CustomTimer;
 import com.min01.tickrateapi.util.TickrateUtil;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 
 import net.minecraft.Util;
+import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleEngine;
@@ -26,6 +29,19 @@ public class MixinParticleEngine
 {
 	@Shadow
 	protected ClientLevel level;
+	
+	@Inject(at = @At(value = "HEAD"), method = "tick", cancellable = true)
+	private void tick(CallbackInfo ci)
+	{
+		if(TickrateUtil.hasDimensionTimer(this.level.dimension()))
+		{
+			CustomTimer timer = TickrateUtil.getDimensionTimer(this.level.dimension());
+			if(timer.tickrate == 0.0F)
+			{
+				ci.cancel();
+			}
+		}
+	}
 	
 	@Inject(at = @At(value = "HEAD"), method = "tickParticleList", cancellable = true)
 	private void tickParticleList(Collection<Particle> particles, CallbackInfo ci)
@@ -57,6 +73,19 @@ public class MixinParticleEngine
 					}
 				}
 			}
+		}
+	}
+	
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/Particle;render(Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/client/Camera;F)V"), method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V")
+	private void render(Particle instance, VertexConsumer p_107261_, Camera p_107262_, float p_107263_)
+	{
+		if(TickrateUtil.hasDimensionTimer(this.level.dimension()))
+		{
+			instance.render(p_107261_, p_107262_, TickrateUtil.getDimensionTimer(this.level.dimension()).partialTick);
+		}
+		else
+		{
+			instance.render(p_107261_, p_107262_, p_107263_);
 		}
 	}
 	
